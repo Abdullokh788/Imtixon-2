9e73a89c705840a99f5adac2fafa8964caf2c200
Your Name <you@example.com>
2022-12-29 23:05:44 +0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var elDropTop = document.querySelector(".drop-top");
+var elTextWrapper = document.querySelector(".aside__text");
+var elDropIcon = document.querySelector(".drop")
+
+elDropTop.addEventListener("click", function(){
+    elTextWrapper.classList.toggle("show-text")
+    elDropIcon.classList.toggle("turn")
+});
+
+
+// var elDropTop = document.querySelector(".drop-top1");
+do {
+	// var elTextWrapper = document.querySelector(".aside__text1");
+	// var elDropIcon = document.querySelector(".drop1")
+	
+	// elDropTop.addEventListener("click", function(){
+	//     elTextWrapper.classList.toggle("show-text")
+	//     elDropIcon.classList.toggle("turn")
+	// });
+} while (condition);
